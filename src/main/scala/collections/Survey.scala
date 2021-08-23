ef6c141ee5c81818e76080839f480754a690d8f8
Oleksii Hubrii <oleksii.hubrii@gmail.com>
2021-08-23 18:51:10 +0300

    change representation of rows

--- src/main/scala/collections/Survey.scala
+++ src/main/scala/collections/Survey.scala
@@ -1,34 +1,51 @@
 package collections
 
+import collections.Activity.{Activity, All, Leisure, PrimaryNeeds, Work}
+
 import scala.io.Source
 
 object Survey extends App {
-  case class Row(values: Map[String, String])
-
   val lines = Source.fromResource("atussum.csv").getLines.take(10000)
   val headers = lines.next.split(",").map(_.trim).toList
-  val rows = lines.toList.map(_.split(",")).map(r => Row((headers zip r).toMap))
 
   val primaryNeedsColumns = findColumns(List("t01", "t03", "t11", "t1801", "t1803"))
   val workingColumns = findColumns(List("t05", "t1805"))
   val leisureColumns = findColumns(List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18"))
     .filterNot(i => primaryNeedsColumns.contains(i) || workingColumns.contains(i))
 
+  val respondents = lines.toList.map(_.split(",")).map(row => (headers zip row).toMap).map(entry => {
+    val primaryNeeds = entry.filter(cn => primaryNeedsColumns.contains(cn._1)).map(_._2.toDouble).sum
+    val work = entry.filter(cn => workingColumns.contains(cn._1)).map(_._2.toDouble).sum
+    val leisure = entry.filter(cn => leisureColumns.contains(cn._1)).map(_._2.toDouble).sum
+    Respondent(entry("tesex").toInt, entry("teage").toInt, entry("telfs").toInt, primaryNeeds, work, leisure)
+  })
+
   printf("Average time spent on primary needs compared to leisure time%n  primary needs - %.2f%n  leisure time - %.2f%n%n",
-    calculateTime(primaryNeedsColumns, _ => true), calculateTime(leisureColumns, _ => true))
+    calculateTime(_ => true, PrimaryNeeds), calculateTime(_ => true, Leisure))
   printf("Women spend %.2f hours on work while men spend %.2f hours on work%n%n",
-    calculateTime(workingColumns, _("tesex").toInt != 1), calculateTime(workingColumns, _("tesex").toInt == 1))
+      calculateTime(_.sex != 1, Work), calculateTime(_.sex == 1, Work))
   printf("Time spent on primary needs%n  young people - %.2f%n  active people - %.2f%n  elder people - %.2f%n%n",
-    calculateTime(primaryNeedsColumns, m => { val a = m("teage").toInt; 15 <= a && a <= 22 }),
-    calculateTime(primaryNeedsColumns, m => { val a = m("teage").toInt; 23 <= a && a <= 55 }),
-    calculateTime(primaryNeedsColumns, m => { val a = m("teage").toInt; a > 55 }))
+    calculateTime(r => 15 <= r.age && r.age <= 22, PrimaryNeeds), calculateTime(r => 23 <= r.age && r.age <= 55, PrimaryNeeds), calculateTime(_.age > 55, PrimaryNeeds))
   printf("Leisure time of working people compared to unemployed people:%n  working people - %.2f%n  unemployed people - %.2f%n",
-    calculateTime(leisureColumns, m => { val i = m("telfs").toInt; 1 <= i && i < 3 }), calculateTime(leisureColumns, _("telfs").toInt == 5))
+    calculateTime(r => 1 <= r.workStatus && r.workStatus < 3, Leisure), calculateTime(_.workStatus == 5, Leisure))
 
   def findColumns(columns: List[String]): List[String] = headers.filter(h => columns.exists(cn => h.startsWith(cn)))
 
-  def calculateTime(columns: List[String], p: Map[String, String] => Boolean): Double = {
-    val times = rows.filter(r => p(r.values)).map(_.values.view.filterKeys(columns.contains).toMap.map(_._2.toDouble).sum)
-    times.sum / times.length
+  def calculateTime(p: Respondent => Boolean, activity: Activity): Double = {
+    val res = respondents.filter(p)
+    val totalTime = activity match {
+      case PrimaryNeeds => res.map(_.primaryNeeds)
+      case Work => res.map(_.work)
+      case Leisure => res.map(_.leisure)
+      case All => res.map(r => r.primaryNeeds + r.work + r.leisure)
+    }
+    totalTime.sum / res.length
   }
+}
+
+case class Respondent(sex: Int, age: Int, workStatus: Int, primaryNeeds: Double, work: Double, leisure: Double)
+
+object Activity extends Enumeration {
+  type Activity = Value
+  val PrimaryNeeds, Work, Leisure, All = Value
 }
