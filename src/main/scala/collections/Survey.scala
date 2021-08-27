dfdfe3169b98884edbd40f8677a987d3393eb0db
Oleksii Hubrii <oleksii.hubrii@gmail.com>
2021-08-27 11:20:58 +0300

    change case class Respondent

--- src/main/scala/collections/Survey.scala
+++ src/main/scala/collections/Survey.scala
@@ -1,6 +1,9 @@
 package collections
 
 import collections.Activity.{Activity, All, Leisure, PrimaryNeeds, Work}
+import collections.Age.{Age, Active, Elder, Young}
+import collections.Employability.{Employability, Unemployed, Working}
+import collections.Sex.{Sex, Female, Male}
 
 import scala.io.Source
 
@@ -23,11 +26,11 @@ object Survey extends App {
   printf("Average time spent on primary needs compared to leisure time%n  primary needs - %.2f%n  leisure time - %.2f%n%n",
     calculateTime(_ => true, PrimaryNeeds), calculateTime(_ => true, Leisure))
   printf("Women spend %.2f hours on work while men spend %.2f hours on work%n%n",
-      calculateTime(_.sex != 1, Work), calculateTime(_.sex == 1, Work))
+      calculateTime(_.sex == Female, Work), calculateTime(_.sex == Male, Work))
   printf("Time spent on primary needs%n  young people - %.2f%n  active people - %.2f%n  elder people - %.2f%n%n",
-    calculateTime(r => 15 <= r.age && r.age <= 22, PrimaryNeeds), calculateTime(r => 23 <= r.age && r.age <= 55, PrimaryNeeds), calculateTime(_.age > 55, PrimaryNeeds))
+    calculateTime(_.age == Young, PrimaryNeeds), calculateTime(_.age == Active, PrimaryNeeds), calculateTime(_.age == Elder, PrimaryNeeds))
   printf("Leisure time of working people compared to unemployed people:%n  working people - %.2f%n  unemployed people - %.2f%n",
-    calculateTime(r => 1 <= r.workStatus && r.workStatus < 3, Leisure), calculateTime(_.workStatus == 5, Leisure))
+    calculateTime(_.workStatus == Working, Leisure), calculateTime(_.workStatus == Unemployed, Leisure))
 
   def findColumns(columns: List[String]): List[String] = headers.filter(h => columns.exists(cn => h.startsWith(cn)))
 
@@ -43,9 +46,36 @@ object Survey extends App {
   }
 }
 
-case class Respondent(sex: Int, age: Int, workStatus: Int, primaryNeeds: Double, work: Double, leisure: Double)
+case class Respondent(sex: Sex, age: Age, workStatus: Employability, primaryNeeds: Double, work: Double, leisure: Double)
+
+object Respondent {
+  def apply(sex: Int, age: Int, workStatus: Int, primaryNeeds: Double, work: Double, leisure: Double): Respondent = {
+    val sexEnum = if (sex == 1) Male else Female
+    val ageEnum =
+      if (15 <= age && age <= 22) Young
+      else if (23 <= age && age <= 55) Active
+      else Elder
+    val workEnum = if (1 <= workStatus && workStatus < 3) Working else Unemployed
+    new Respondent(sexEnum, ageEnum, workEnum, primaryNeeds, work, leisure)
+  }
+}
 
 object Activity extends Enumeration {
   type Activity = Value
   val PrimaryNeeds, Work, Leisure, All = Value
+}
+
+object Sex extends Enumeration {
+  type Sex = Value
+  val Male, Female = Value
+}
+
+object Age extends Enumeration {
+  type Age = Value
+  val Young, Active, Elder = Value
+}
+
+object Employability extends Enumeration {
+  type Employability = Value
+  val Working, Unemployed = Value
 }
