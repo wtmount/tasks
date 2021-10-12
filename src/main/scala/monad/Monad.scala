6f5e66f4c51ca0e69ff541ee3fedfd00bbd86e63
Oleksii Hubrii <oleksii.hubrii@gmail.com>
2021-10-12 11:31:07 +0300

    change IOMonad implementation

--- src/main/scala/monad/Monad.scala
+++ src/main/scala/monad/Monad.scala
@@ -2,39 +2,41 @@ package monad
 
 import scala.io.StdIn.readLine
 
-trait Monad[M[_]] {
-  def unit[A](a: A): M[A]
-  def flatMap[A, B](m: M[A])(f: A => M[B]): M[B]
-}
-
-object Monad {
-  def unit[A, M[_] : Monad](a: A): M[A] = implicitly[Monad[M]].unit(a)
-
-  implicit class MonadOps[A, M[_] : Monad](m: M[A])  {
-    def flatMap[B](f: A => M[B]): M[B] = implicitly[Monad[M]].flatMap(m)(f)
-  }
-}
-
-class IO[A](a: => A) {
-  def run: A = a
-  def map[B](f: A => B): IO[B] = new IO(f(run))
-  def flatMap[B](f: A => IO[B]): IO[B] = new IO(f(run).run)
-}
-
-object IOMonad extends Monad[IO] {
-  override def unit[A](a: A): IO[A] = new IO[A](a)
-  override def flatMap[A, B](m: IO[A])(f: A => IO[B]): IO[B] = m.flatMap(f)
-
-  def output(s: String): IO[Unit] = new IO[Unit](println(s))
-  def input(): IO[String] = new IO[String](readLine())
-}
-
 object IOApp extends App {
-  val a = for {
-    _ <- IOMonad.output("enter a string")
-    s <- IOMonad.input()
-    _ <- IOMonad.output(s)
-  } yield s
+  trait Monad[M[_]] {
+    def unit[A](a: A): M[A]
+    def map[A, B](m: M[A])(f: A => B): M[B]
+    def flatMap[A, B](m: M[A])(f: A => M[B]): M[B]
+  }
 
-  a.run
+  object Monad {
+    def unit[A, M[_] : Monad](a: A): M[A] = implicitly[Monad[M]].unit(a)
+    implicit class MonadOps[A, M[_] : Monad](m: M[A]) {
+      def map[B](f: A => B): M[B] = implicitly[Monad[M]].map(m)(f)
+      def flatMap[B](f: A => M[B]): M[B] = implicitly[Monad[M]].flatMap(m)(f)
+    }
+  }
+
+  class IO[A](a: => A) { def run: A = a }
+
+  def output(line: String): IO[Unit] = new IO(println(line))
+  def input(): IO[String] = new IO(readLine)
+
+  implicit object IOMonad extends Monad[IO] {
+    def unit[A](a: A): IO[A] = new IO(a)
+    def map[A, B](m: IO[A])(f: A => B): IO[B] = new IO(f(m.run))
+    def flatMap[A, B](m: IO[A])(f: A => IO[B]): IO[B] = new IO(f(m.run).run)
+  }
+
+  import Monad._
+
+//  val line = output("Input line:") flatMap (_ => input()) flatMap output
+//  line.run
+
+  val line = for {
+    _ <- output("Input line:")
+    line <- input()
+    _ <- output(line)
+  } yield line
+  line.run
 }
